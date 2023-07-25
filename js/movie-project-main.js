a8b03184338ca3aac39c5114b631a2cdf0cb9add
Samantha Ramos <Samanatha.Ramos01@gmail.com>
2023-07-25 18:25:59 -0500

    added functionality to the remove button

--- js/movie-project-main.js
+++ js/movie-project-main.js
@@ -182,7 +182,12 @@ const renderFavoriteMovies = async (favoritesParam) => {
                         </div>
                         <div class="backSide">
                             <div>
-                                <button class="remove-from-favorites">X</button>
+                            <!-- this is the trash can button -->
+                                <button class="remove-from-favorites" ><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                                <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"/>
+                                <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"/>
+                                </svg></button>
+                            <!-- this is the end of the trash can button-->
                                 <h2 class="backSide-text-color">${favorite.original_title}</h2>
                             </div>
                             <p>${favorite.genre_names.join(", ")}</p>
