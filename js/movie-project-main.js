2fdd2bd8ac93bbf86fc1f5e3f4777b843ce58fd6
Cesar Vallin <cesarvallin.cv@gmail.com>
2023-07-24 19:48:30 -0500

    refactored fetch & then with async and await

--- js/movie-project-main.js
+++ js/movie-project-main.js
@@ -4,33 +4,11 @@ console.log(`hello from movie-project-main.js`);
 const DOMAIN = 'http://localhost:3000';
 // fire up json server: json-server --watch data/db.json
 
-// Fetch & .then
 
 
 
-// commented out are the original versions
-// GETTER
-// Get Movies by Search function:
-// function getMoviesBySearch (queryParam) {
-//     const baseUrl = 'https://api.themoviedb.org/3/search/movie';
-//     const queryString = `?query=${encodeURIComponent(queryParam)}&api_key=${TMDB_API}`;
-//     const url = baseUrl + queryString;
-//     const options = {
-//         method: 'GET',
-//         headers: {
-//             accept: 'application/json',
-//             Authorization: TMDB_API
-//         }
-//     }
-//     return fetch(url, options)
-//         .then(response => {
-//             return response.json();
-//         })
-//         .catch(error => {
-//             console.log(error);
-//         })
-// }
 
+// GETTER Get Movies by Search function:
 async function getMoviesBySearch(queryParam) {
     const baseUrl = 'https://api.themoviedb.org/3/search/movie';
     const queryString = `?query=${encodeURIComponent(queryParam)}&api_key=${TMDB_API}`;
@@ -45,7 +23,8 @@ async function getMoviesBySearch(queryParam) {
 
     try {
         const response = await fetch(url, options);
-        return await response.json();
+        const data = await response.json();
+        return data;
     } catch (error) {
         console.log(error);
     }
@@ -53,28 +32,6 @@ async function getMoviesBySearch(queryParam) {
 
 
 // This gives me access to all the movies.  It was done with the discover filter, and the read access token
-// function discoverAllMovies() {
-//     const urlDiscover = 'https://api.themoviedb.org/3/discover/movie';
-//     const optionsDiscover = {
-//         method: 'GET',
-//         headers: {
-//             accept: 'application/json',
-//             Authorization: `Bearer ${TMDB_API_READ_ACCESS_TOKEN}`
-//         }
-//     }
-//
-//     return fetch(urlDiscover, optionsDiscover)
-//         .then(response => {
-//             return response.json();
-//         })
-//         .then(data => {
-//             return data;
-//         })
-//         .catch(error => {
-//             console.log(error);
-//         })
-// }
-
 async function discoverAllMovies() {
     const urlDiscover = 'https://api.themoviedb.org/3/discover/movie';
     const optionsDiscover = {
@@ -95,28 +52,8 @@ async function discoverAllMovies() {
 }
 
 
+// Fetch genreList
 // This gives me access to all genres, genre id & genre name.  Turn this into a function? Declare it as a variable to use and map() corresponding genres?
-// function genreList() {
-//     const urlGenreList = 'https://api.themoviedb.org/3/genre/movie/list';
-//     const optionsGenreList = {
-//         method: 'GET',
-//         headers: {
-//             accept: 'application/json',
-//             Authorization: `Bearer ${TMDB_API_READ_ACCESS_TOKEN}`
-//         }
-//     }
-//     return fetch(urlGenreList, optionsGenreList)
-//         .then(response => {
-//             return response.json();
-//         })
-//         .then(data => {
-//             return data;
-//         })
-//         .catch(error => {
-//             console.log(error);
-//         })
-// }
-
 async function genreList() {
     const urlGenreList = 'https://api.themoviedb.org/3/genre/movie/list';
     const optionsGenreList = {
@@ -129,74 +66,27 @@ async function genreList() {
 
     try {
         const response = await fetch(urlGenreList, optionsGenreList);
-        if (!response.ok) {
-            throw new Error('Failed to fetch genre list');
-        }
         const data = await response.json();
         return data;
     } catch (error) {
         console.log(error);
-        return null;
     }
 }
 
-// function getGenreName (genres, id) {
-//     // Find genre object in the genres array
-//     let genre = genres.find(genre => genre.id === id);
-//
-//     // Check if a genre was found, if not return a default value
-//     if (!genre) {
-//         return "Genre not found";
-//     }
-//
-//     // Return the genre's name
-//     return genre.name;
-// }
+// Function to be used within map() `genre ids to genre names`
+function getGenreName (genres, id) {
+    // Find genre object in the genres array
+    let genre = genres.find(genre => genre.id === id);
 
-function getGenreName(genres, id) {
-    // Find genre object in the genres array and use optional chaining to handle the possibility of undefined
-    const genre = genres.find(genre => genre.id === id)?.name;
+    // Check if a genre was found, if not return a default value
+    if (!genre) {
+        return "Genre not found";
+    }
 
-    // Return the genre's name or a default value if genre is not found
-    return genre || "Genre not found";
+    // Return the genre's name
+    return genre.name;
 }
 
-// ------------------------------------------------------------------------------------------------
-// IIFE...
-// (() => {
-//     // Variables
-//     const searchBar = document.querySelector('#search-bar');
-//     // Functions...
-//     discoverAllMovies().then(data => {
-//         console.log(data);
-//     });
-//     genreList().then(data => {
-//         console.log(data);
-//     })
-//
-//     genreList().then(genres => {
-//         // Events....
-//         searchBar.addEventListener('keyup', (e) => {
-//             if(e.keyCode === 13) {
-//                 console.log(searchBar.value);
-//                 getMoviesBySearch(searchBar.value).then(moviesData => {
-//                     searchBar.value = '';
-//                     console.log(`Fetch & then => `, moviesData);
-//                     const firstFilter = moviesData.results.filter((movie) => {
-//                         return movie.original_language === 'en' && movie.backdrop_path !== null;
-//                     }).map((movie) => {
-//                         return {
-//                             ...movie,
-//                             genre_names: movie.genre_ids.map(id => getGenreName(genres.genres, id))
-//                         }
-//                     });
-//                     console.log(`firstFilter => `, firstFilter);
-//                 })
-//             }
-//         })
-//     })
-//
-// })();
 
 (async () => {
     // Variables
@@ -240,7 +130,6 @@ function getGenreName(genres, id) {
         });
     });
 })();
-
 
 // ------------------------------------------------------------------------------------------------
 // Delete favMovie function...
