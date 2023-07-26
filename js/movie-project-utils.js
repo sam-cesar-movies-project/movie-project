2dc06a6031a45d052a30d12324a7f09af329d71e
Cesar Vallin <cesarvallin.cv@gmail.com>
2023-07-26 10:53:57 -0500

    up to date

--- js/movie-project-utils.js
+++ js/movie-project-utils.js
@@ -270,7 +270,9 @@ export const renderSearchedMovies = async (filterParam) => {
                             <p>${result.genre_names.join(", ")}</p>
                             <p class="backSide-text-color">Popularity: ${result.popularity}</p>
                             <p class="overview">${result.overview}</p>
-                            <button class="add-to-favorites">Add to Favorites</button>
+                            <button class="add-to-favorites"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-lg" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z"/>
+                            </svg></button>
                         </div>
                     </div>
                 </div>
